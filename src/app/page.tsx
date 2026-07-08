611a5fbff264c79009c2be7521ccd38d54c04808
Chandrashekhar <chandrashekharcorp@gmail.com>
2026-07-08 07:00:40 +0530

    added till image scalling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,155 @@
+'use client';
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import { SplitText } from "gsap/SplitText"
+import { useEffect, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+
 export default function Home() {
+  const spotlightGallery = useRef<HTMLDivElement | null>(null);
+  const logo = useRef<HTMLDivElement | null>(null);
+  const heroFooter = useRef<HTMLDivElement | null>(null);
+  const heroInner = useRef<HTMLDivElement | null>(null);
+  const heroOverlay = useRef<HTMLDivElement | null>(null);
+  const btn = useRef<HTMLAnchorElement | null>(null);
+  const heroh3 = useRef<HTMLHeadingElement | null>(null);
+
+
+
+
+  const lerp = (from:number, to:number, t:number) => from + (to - from) * t;
+  const mapRange = (value:number, rangeStart:number, rangeEnd:number) =>
+    gsap.utils.clamp(0, 1, (value - rangeStart) / (rangeEnd - rangeStart));
+  
+  useEffect(()=>{
+    if(!heroh3.current) return;
+
+    const headlineSplit = new SplitText(heroh3.current,{
+      type:"words",
+      wordsClass:"words",
+    })
+    
+    const headerfadeTarget = [...headlineSplit.words, heroh3.current!];
+    gsap.set(headerfadeTarget, {opacity:0});
+
+    const headerFadeStep = (0.6 - 0.1) / headerfadeTarget.length;
+    const headerFadeDuration = headerFadeStep * 3;
+  },[])
+
+
+  // const MOBILE_BREAKPOINT = 1000;
+
+  // let logoStartScale = 6;
+  // gsap.matchMedia().add(`(max-width: ${MOBILE_BREAKPOINT}px)`,()=>{
+  //   logoStartScale = 2;
+  //   return () => (logoStartScale = 6);
+  // })
+
+  // ScrollTrigger.create({
+  //   trigger: ".hero",
+  //   start:"top top",
+  //   end: `*=${window.innerHeight * 4}px`,
+  //   pin: true,
+  //   pinSpacing:false,
+  //   onUpdate: (self) =>{
+  //     const scrollProgress = self.progress;
+
+  //     const galleryProgress = mapRange(scrollProgress, 0, 0.75);
+  //     const galleryScale = lerp(1, 0.5, galleryProgress);
+  //     gsap.set(spotlightGallery, {scale:galleryScale});
+  //   }
+  // })
+
+
+  const heroRef = useRef<HTMLElement | null>(null);
+
+  useGSAP(() => {
+
+    if (!spotlightGallery.current) return;
+    
+    const images = spotlightGallery.current.querySelectorAll(
+      ".hero-spotlight-item img"
+    );
+    let logoStartScale = 6;
+
+    const mm = gsap.matchMedia();
+
+    mm.add("(max-width:1000px)", () => {
+        logoStartScale = 2;
+
+        return () => {
+            logoStartScale = 6;
+        };
+    });
+
+    const trigger = ScrollTrigger.create({
+
+        trigger: heroRef.current,
+
+        start: "top top",
+
+        end: () => `+=${window.innerHeight * 4}`,
+
+        pin: true,
+
+        pinSpacing: false,
+
+        onUpdate: (self) => {
+
+            const scrollProgress = self.progress;
+
+            const galleryProgress =
+                gsap.utils.mapRange(
+                    0,
+                    0.75,
+                    0,
+                    1,
+                    scrollProgress
+                );
+
+            const galleryScale =
+                lerp(
+                    1,
+                    0.5,
+                    galleryProgress
+                );
+
+            gsap.set(spotlightGallery.current, {
+                scale: galleryScale,
+            });
+
+
+            const imageScale = lerp(1.25, 1, galleryProgress);
+            gsap.set(images, {scale:imageScale});
+
+        },
+
+    });
+
+    return () => {
+
+        trigger.kill();
+
+        mm.revert();
+
+    };
+
+}, {
+    scope: heroRef,
+});
+
+
+
+
+
   return (
     <>
-      <div className="logo">
+      <div ref={logo} className="logo">
 
         <img src="/logo.svg" alt="" />
 
@@ -9,10 +157,10 @@ export default function Home() {
       </div>
 
 
-      <section className="hero">
+      <section ref={heroRef} className="hero">
 
-        <div className="hero-inner">
-          <div className="hero-spotlight-gallery">
+        <div ref={heroInner} className="hero-inner">
+          <div ref={spotlightGallery} className="hero-spotlight-gallery">
 
             <div className="hero-spotlight-col">
               <div className="hero-spotlight-item">
@@ -58,19 +206,20 @@ export default function Home() {
 
 
           </div>
+
           <div className="hero-header">
-            <h3>
-              A living catalogue of images but shouldn't exist. 
+            <h3 ref={heroh3}>
+              A living catalogue of images but shouldn't exist.
               Collected frame by frame from the edge of the real.
             </h3>
-            <a href="#" className="btn">Request Access</a>
+            <a ref={btn} href="#" className="btn">Request Access</a>
           </div>
 
-          <div className="hero-footer">
+          <div ref={heroFooter} className="hero-footer">
             <h5>An archive of the unreal</h5>
           </div>
         </div>
-        <div className="hero-overlay"></div>
+        <div ref={heroOverlay} className="hero-overlay"></div>
       </section>
 
 
